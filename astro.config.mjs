251ff687167fd1d6ea8244e06287561ab30e38fb
Joey Yu <joey@itsjoeoui.com>
2023-05-07 19:35:43 -0400

    Enable vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -13,5 +12,7 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    analytics: true,
+  }),
 });
