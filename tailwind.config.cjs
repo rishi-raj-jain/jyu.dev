46619bd889727bd71621bfddf47fa2cce6a9fb33
Joey Yu <joey@itsjoeoui.com>
2023-05-07 18:14:54 -0400

    Update time

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "475px",
+        "3xl": "1600px",
+        "4xl": "1800px",
+      },
+    },
+  },
+  plugins: [],
+};
